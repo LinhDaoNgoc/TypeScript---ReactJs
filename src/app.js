0f8564051b27377996c4b06ee6e1b447e34f4c24
LinhDaoNgoc <daongoclinh22@gmail.com>
2022-03-09 07:26:21 +0700

    comit new

--- src/app.js
+++
@@ -1,6 +0,0 @@
-var a = 20;
-var b = 20;
-function sum(numA, numB) {
-    return numA + numB;
-}
-sum(a, b);
--- src/app.ts
+++ src/app.ts
@@ -1,7 +1,33 @@
+// export{}
+
+type Product = {
+    id: number;
+    name: string;
+    status: boolean;
+}
+
+//tsc --init 
+//target: "es2018"
+// baseUrl: './src'
+//outDir: './dist'
+//gox cau lenh : tsc --watch
+
+
 const a:number = 20;
 const b:number = 20;
+const name: string | number = "dao ngoc linh";
+const age:number = 20;
+const status: boolean = true;
+const info:  Product[] = [{id: a}, {id: b}]
+const myArr: string[] = ["a", "b", "c"];
+const numberArr: number[] = [1,2,3,4,5];
+const objectArr: Product[] = [{id: 1}, {id: 2}]
+
+console.log(`Name:${name} Age:${age} Status:${status} MyArr:${myArr}`);
+
 
 function sum(numA: number, numB: number): number{
     return numA + numB
 }
 sum(a,b);
+ //tsc --watch duong_dan_file
