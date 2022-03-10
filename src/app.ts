2898de0cd2ad4a2dd84c426fd6c07654aac3f285
LinhDaoNgoc <daongoclinh22@gmail.com>
2022-03-10 19:36:13 +0700

    Update Thursday 10/03

--- src/app.ts
+++ src/app.ts
@@ -1,33 +1,29 @@
-// export{}
+export {};
 
 type Product = {
-    id: number;
-    name: string;
-    status: boolean;
-}
-
-//tsc --init 
-//target: "es2018"
+  id: number;
+  name: string;
+  status?: boolean; // required
+};
+// tsc --init
+// target: "es2018"
 // baseUrl: './src'
-//outDir: './dist'
-//gox cau lenh : tsc --watch
-
-
-const a:number = 20;
-const b:number = 20;
-const name: string | number = "dao ngoc linh";
-const age:number = 20;
+// ourDir: './dist'
+// gõ câu lệnh : tsc --watch
+const a: number = 20;
+const b: number = 20;
+const name: string | number = 20; // union
+const age: number = 20;
 const status: boolean = true;
-const info:  Product[] = [{id: a}, {id: b}]
-const myArr: string[] = ["a", "b", "c"];
-const numberArr: number[] = [1,2,3,4,5];
-const objectArr: Product[] = [{id: 1}, {id: 2}]
+const info: Product = { id: 1, name: "a" };
+const stringArr: string[] = ["a", "b", "c"];
+const numberArr: number[] = [1, 2, 3, 4];
+const objectArr: Product[] = [{ id: 1 }, { id: 2 }];
 
-console.log(`Name:${name} Age:${age} Status:${status} MyArr:${myArr}`);
-
-
-function sum(numA: number, numB: number): number{
-    return numA + numB
+function sum(numA: number, numB: number): number {
+  return numA + numB;
+  // return numA + numB;
 }
-sum(a,b);
- //tsc --watch duong_dan_file
+
+sum(a, b);
+// tsc --watch ten_file
